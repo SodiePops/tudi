5e8cc6c9b66304804dce8d6319290627c5c0fc50
Ian J Sikes <ianjsikes@gmail.com>
2017-05-04 14:47:15 -0700

    Add mouse xy position to input module

--- src/Util/Mouse.ts
+++ src/Util/Mouse.ts
@@ -2,6 +2,8 @@
  * Mouse input module
  */
 const pressed: { [keyCode: number]: boolean } = {}
+export let mouseX: number = 0
+export let mouseY: number = 0
 
 const onMouseDown = (event: MouseEvent): void => {
   pressed[event.button] = true
@@ -12,8 +14,14 @@ const onMouseUp = (event: MouseEvent): void => {
   delete pressed[event.button]
 }
 
+const onMouseMove = (event: MouseEvent): void => {
+  mouseX = event.clientX
+  mouseY = event.clientY
+}
+
 window.addEventListener('mouseup', onMouseUp, false)
 window.addEventListener('mousedown', onMouseDown, false)
+window.addEventListener('mousemove', onMouseMove, false)
 
 export const isDown = (button: number): boolean => {
   return pressed[button]
