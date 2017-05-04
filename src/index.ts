8812e6c5d82251ff231b7a390096ffb195b49942
Ian J Sikes <ianjsikes@gmail.com>
2017-05-04 09:33:06 -0700

    Add keyboard input module

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import * as Components from './Components'
 import Entity from './Entity'
 import Game from './Game'
 import Scene from './Scene'
+import * as Keyboard from './Util/Keyboard'
 
 export {
   Game,
   Scene,
   Entity,
-  Components
+  Components,
+  Keyboard,
 }
