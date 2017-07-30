0786533a3c2b0340dd148a578ade280d6ccb9885
Ian J Sikes <ianjsikes@gmail.com>
2017-07-30 12:05:46 -0700

    Implement custom asset parsers in laslo, remove old resource loader code

--- src/Util/ResourceManager.ts
+++
@@ -1,7 +0,0 @@
-import * as PIXI from 'pixi.js'
-
-export function loadResources(resources: string[]): Promise<{}> {
-  return new Promise((resolve: () => void): void => {
-    PIXI.loader.add(resources).load(resolve)
-  })
-}
