0b217062804932266c84aa852854e318f5033f62
Ian J Sikes <ianjsikes@gmail.com>
2017-07-30 12:13:50 -0700

    Remove old resourceManager code

--- src/Scene.ts
+++ src/Scene.ts
@@ -1,15 +1,8 @@
 import { Game } from './Game'
 import Entity from './Entity'
-import * as ResourceManager from './Util/ResourceManager'
-import * as AudioManager from './Util/AudioManager'
 import ActionChannel from './Util/ActionChannel'
 import { Camera } from './Components/camera'
 import * as most from 'most'
-
-export interface SceneResources {
-  images?: string[]
-  sounds?: AudioManager.SoundProperties[]
-}
 
 /**
  * A Scene is the root of a hierarchy of entities.
@@ -22,15 +15,12 @@ export default class Scene {
   cameras: Camera[] = []
   entityCount: number
   entities: { [name: string]: Entity } = {}
-  resources: SceneResources
   update$: most.Stream<number>
   actions: ActionChannel = new ActionChannel()
-
-  constructor(resources: SceneResources, entities: Entity[]) {
+  constructor(entities: Entity[]) {
     for (const entity of entities) {
       this.entities[entity.id] = entity
     }
-    this.resources = resources
   }
 
   addEntity(e: Entity): void {
@@ -54,21 +44,20 @@ export default class Scene {
       for (const camera of this.cameras) {
         camera.draw()
       }
-
       // Clear each shader's render queue
       for (const shader of Game.shaders) {
         shader.renderQueue = []
       }
-
       return dt
     })
-
-    await ResourceManager.loadResources(this.resources.images)
-    await AudioManager.loadSounds(this.resources.sounds)
 
     for (const entity of Object.values(this.entities)) {
       entity.scene = this
       entity.setup()
     }
+  }
+
+  async destroy() {
+    this.update$ = null
   }
 }
